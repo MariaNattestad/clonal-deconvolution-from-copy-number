4ecf0002839004cfc74abf0ceed73d38dff2d8e0
MariaNattestad <lunexa@gmail.com>
2014-07-25 12:38:43 -0400

    Fixed memory issues by keeping only the best 10 solutions as runs proceed

--- deconvolve.py
+++ deconvolve.py
@@ -6,10 +6,11 @@ from scipy import *
 from numpy.linalg import *
 import numpy
 import time
+import os
 
 def collect_costs(outdir):
     import os
-    
+
     costs = dict()
     done = False
     num=2
@@ -32,7 +33,7 @@ def collect_costs(outdir):
                 else:
                     #print "%s is not a file" % filename
                     list_done=True
-            
+
                 soln+=1
 
             costs[num]=cost_list
@@ -41,17 +42,17 @@ def collect_costs(outdir):
         else:
             done=True
             #print "%s is not a directory" % directory
-    
+
     f_costs=open("%s/costs.txt" % outdir,'w')
     f_min_costs=open("%s/min_costs.txt" % outdir,'w')
-    
+
     for i in xrange(2,maxnum+1):
         mylist=costs[i]
-        
+
         for j in xrange(len(mylist)):
             f_costs.write("%s\t" % (mylist[j]))
         f_costs.write("\n")
-        
+
         f_min_costs.write("%d,%s\n" % (i,mylist[0]))
     f_costs.close()
     
@@ -65,15 +66,15 @@ def check_file(filename,outdir):
     numbins=D.shape[1]
     print "Data indicates %d samples, and the genome is split into %d bins" % (numsignals,numbins)
     print "(If this is false, make sure the input file has the samples as rows and the bins as columns)"
-    
+
     if numsignals < 2:
         print "There must be at least 2 samples from a tumor to run this program."
-    
+
     f = open('%s/info.txt' % outdir,'w')
     f.write("samples\t%d \nbins\t%d" % (numsignals,numbins))
     f.write("\n")
     f.close()
-    
+
 def read_info(filename):
     f = open(filename,'r')
     numsamples = 0
@@ -89,21 +90,27 @@ def read_info(filename):
     return numsamples, numbins
 
 def run_deconvolve_from_file(filename,outdir,numclones=2,testing=False,progress_file=""):
+    import os
     if progress_file != "":
         print "Printing progress to %s" % progress_file
-    
+        
+        if os.path.exists(progress_file)==False:
+            f=open(progress_file,'w')
+            f.write("")
+            f.close()
+
     if testing=="False" or testing=="false":
-	testing=False
+        testing=False
     elif testing=="True" or testing=="true":
-	testing=True
+        testing=True
     else:
-	print "cannot understand %s: expecting True/true or False/false" % testing
-    
-    
+        print "cannot understand testing=%s: expecting True/true or False/false" % testing
+
+
     D=loadmatrix(filename)
     print D.shape
     numclones = int(numclones)
-    costs,allS,allR = deconvolve(D,numclones,testing=testing,progress_file=progress_file)
+    costs,allS,allR,count_occurrence = deconvolve(D,numclones,testing=testing,progress_file=progress_file)
     #print best_R
     #print costs.shape
     #print allS.shape
@@ -114,34 +121,19 @@ def run_deconvolve_from_file(filename,outdir,numclones=2,testing=False,progress_
     sorted_costs = costs[indices]
     sorted_S = allS[indices]
     sorted_R = allR[indices]
-    
-    cost_set = list()
-    S_set = list()
-    R_set = list()
-    for i in xrange(len(sorted_costs)):
-        
-        c =  sorted_costs[i]
-        S = sorted_S[i]
-        R = sorted_R[i]
-        if c not in cost_set:
-            #print "added %f" % c
-            cost_set.append(c)
-            S_set.append(S)
-            R_set.append(R)
-            #print mean(S)
-            
-    print "length of solutions set: %d" % (len(cost_set))
+
+    print "length of solutions set: %d" % (len(sorted_costs))
     num_solns_to_display = 5
-    if num_solns_to_display > len(cost_set):
-        num_solns_to_display = len(cost_set)
+    if num_solns_to_display > len(sorted_costs):
+        num_solns_to_display = len(sorted_costs)
         print "showing all solutions"
-    
+
     ####################################################################################################
     ## still need to check intelligently for duplicate solutions using calc_S_similarity
-    
+
     ####################################################################################################
-    
-    
+
+
     #############################################
     ########### check if outdir exists ##########
     #############################################
@@ -149,50 +141,52 @@ def run_deconvolve_from_file(filename,outdir,numclones=2,testing=False,progress_
     if os.path.isdir(outdir)==False:
         print "Error in deconvolve.py: run_deconvolve_from_file(): outdir %s does not exist" % (outdir)
         return
-    
-    
+
+
     for i in xrange(num_solns_to_display):
-        S = S_set[i]
-        R = R_set[i]
+        S = sorted_S[i]
+        print S.shape
+        R = sorted_R[i]
+        print R.shape
         matrixtofile(S,"%s/S_%d" % (outdir,i), use_float=False,csv=False)
         matrixtofile(R,"%s/R_%d" % (outdir,i), use_float=True,csv=False)
-        
+
         f = open("%s/cost_%d" % (outdir,i),'w')
-        f.write("%.10f" % cost_set[i])
+        f.write("%.10f" % sorted_costs[i])
         f.close()
 
 def loadmatrix(filename):
     # load a matrix from the file
     import numpy as np
     f = open(filename,'r')
-    
+
     content = f.readlines()
     f.close()
-    
+
     #print "%d contigs" % num
     data = []
     for line in content:
         bare=line.strip()
         if (bare != ""):
             data.append(map(float,bare.split()))
-    
+
     data = np.array(data)
-        
-        
+
+
     if data.shape[0]==1:
         data=data[0]
     print data.shape
-    
-    
+
+
     return data
 
 def matrixtofile(X,filename,use_float=True,csv=False):
     # write a matrix to a file with the given filename
-   
+
     delim="\t"
     if csv==True:
         delim=","
-        
+
     if len(X.shape)==2:
         f2 = open(filename,'w')
         for line in X:
@@ -213,141 +207,199 @@ def matrixtofile(X,filename,use_float=True,csv=False):
         f2.close()
     else:
         print "array must be 1- or 2-dimensional"
-    
-def deconvolve(D, numclones, testing=False, max_falling_iterations=15,progress_file=""):
+
+def deconvolve(D, numclones, testing=False, max_falling_iterations=15,progress_file="",outdir=""):
+    if outdir!="":
+        filename="%s/abort" % (outdir)
+        if os.path.exists(filename):
+            print "deconvolve aborted from abort file in output directory"
+            return
+            
     print "in deconvolve()"
     print D.shape
     before = time.time()
     last_update_time=time.time()
-    
+
     numsources=numclones
     numsignals=D.shape[0]
     numbins=D.shape[1]
-    
-    
+
+
     numtrials = 10000
-    
+
     if numsources==4:
         numtrials = 50000
     if numsources==5:
         numtrials = 1000000
-    
+    if numsources > 5:
+        print "CANNOT RUN WITH MORE THAN 5 CLONES. IT WOULD TAKE TOO LONG"
     if testing==True:
-        numtrials=100 
-    
+        numtrials=100
+
     print "running %d random initializations" % (numtrials)
-    
+
     print "number of samples: %d" % numsignals
     print "hypothetical number of clones: %d" % numsources
     print "number of bins in genome: %d" % numbins
+
     
-    costs = []
-    numfalls=[]
-    allS=[]
-    allR=[]
+    #numfalls=[]
+    #costs = []
+    #
+    #allS=[]
+    #allR=[]
+
+    #best_cost=10000000000000
+    #best_R=array([])
+    #best_S=array([])
+    #
+    #allS=array([])
+    #allR=array([])
     
-    best_cost=10000000000000
-    best_R=array([])
-    best_S=array([])
     
-   
-    for i in xrange(numtrials):       
-        
+    
+    
+    
+    num_best_solns_to_store=10
+    
+    
+    best_cost=array([1000000000000000000.0]*num_best_solns_to_store)
+    best_R=[0.0]*num_best_solns_to_store
+    best_S=[0]*num_best_solns_to_store
+    count_occurrence=[0]*num_best_solns_to_store
+    print best_cost
+    print best_R
+    print best_S
+    
+    
+    
+    for i in xrange(numtrials):
+
         R=generate_random_R(numsignals,numsources)
         #S=abs(around((pinv(R).dot(D))))
         S=abs(around(( numpy.dot(pinv(R), D))))
-        
-        
+
+
         iterationcounter=0
         diff=10
-        
+
         while diff > 0.00000001 and iterationcounter < max_falling_iterations:
             previousS=S
-        
+
             #R=D.dot(pinv(S))
             R=numpy.dot(D,pinv(S))
 
-            
+
             ############### Normalize R again ##########################
-            
+
             # normalize rows of R to total 1 again
             R=abs(R)
             row_sums = R.sum(axis=1)
             R = R / row_sums[:, numpy.newaxis]
             ############## Solve for S #################################
             ############## Round S to nearest integers #################
-            
-            
+
+
             # Solve for S and round it to nearest positive integers
             #S=abs(around((pinv(R).dot(D))))
             S=abs(around(( numpy.dot(pinv(R), D))))
-    
+
             ############## Adjust R to fit after S is rounded ##########
-            
-            
-            
-            
+
+
+
+
             diff = calc_S_similarity(previousS,S)
-            
+
             iterationcounter+=1
-        
-        
-        
+
+
+
         #cost = sum((R.dot(S)-D)**2)
         cost = sum((numpy.dot(R,S)-D)**2)
 
-        
 
-        if cost < best_cost:
-            best_cost=cost
-            best_R=R
-            best_S=S
+
+        #if cost < best_cost:
+        #    best_cost=cost
+        #    best_R=R
+        #    best_S=S
         
-        costs.append(cost)
-        numfalls.append(iterationcounter)
-        allS.append(S)
-        allR.append(R)
+        #costs.append(cost)
+        ##numfalls.append(iterationcounter)
+        #allS.append(S)
+        #allR.append(R)
         
-    
-        if i==100:
-            estimate=(time.time()-before)*numtrials/100.0
+        # compare new soln to the costs of the previously stored solutions
+        # store cost if it is better than the worst solution we have stored
+        worst_index=argmax(best_cost)
+        
+        if cost < best_cost[worst_index]: #if the cost matches a previously found solution, just count that we saw that solution again
+            found=False
+            for j in xrange(len(best_cost)):
+                if abs(cost - best_cost[j])<0.000001 and calc_S_similarity(S,best_S[j]) < 0.00000001:
+                    count_occurrence[j]+=1
+                    found=True
+
+            if found==False: # if this is a completely new solution, record it as a replacement for the worst previous
+                best_cost[worst_index]=cost
+                best_R[worst_index]=R
+                best_S[worst_index]=S
+                count_occurrence[worst_index]=1
+        
+        
+        
+        
+        
+        if i==50:
+            estimate=(time.time()-before)*numtrials/50.0
             print "estimated run-time for %d trials: %d seconds or %d minutes" % (numtrials,estimate,estimate/60.0)
-        if (i+1) % (int(numtrials/100))==0:
-            print "progress: %d%%" % ((i+1)*100/numtrials)
+        if (i+1) % (int(numtrials/100.0))==0:
+            percent=(i+1)*100.0/numtrials
+            
+            print "progress: %d%%" % (percent)
             now=time.time()
             if now-last_update_time>1.0: #update every second
-                print "update"
+                print "updating progress.txt"
                 last_update_time=time.time()
                 f=open(progress_file,'a')
                 f.write("clones\t%d\tprogress\t%d\n" % (numclones, i*100.0/numtrials))
                 f.close()
-    
+            if outdir!="":
+                filename="%s/abort" % (outdir)
+                if os.path.exists(filename):
+                    i=numtrials
+                
     f=open(progress_file,'a')
     f.write("clones\t%d\tprogress\t%d\n" % (numclones, 100))
     f.close()
     seconds=time.time()-before
     print "elapsed time: %d minutes, %d seconds" % (seconds/60, seconds%60)
+
+    #costs=array(costs)
+    #allS=array(allS)
+    #allR=array(allR)
     
-    costs=array(costs)
-    allS=array(allS)
-    allR=array(allR)
+    best_cost=array(best_cost)
+    best_S=array(best_S)
+    best_R=array(best_R)
     #return costs, numfalls, best_R, best_cost, best_S,allS
-    return costs,allS,allR
+    return best_cost,best_S,best_R,count_occurrence
 
 def generate_random_R(numsignals,numsources):
     R=rand(numsignals,numsources)
-    
+
     for i in xrange(5):
         jiggle=rand(*R.shape)
         negatives=(rand(*R.shape)<0.5)*-2.0+1 #creates array of -1 and +1 to multiply jiggle array
         R=abs(R+jiggle*negatives)
-        
+
         row_sums = R.sum(axis=1)
         R = R / row_sums[:, numpy.newaxis]
-    
+
     return R
-    
-def generate_random_S(numsources,numbins,distribution=[]): 
+
+def generate_random_S(numsources,numbins,distribution=[]):
     if len(distribution)>0:
         indices=floor(rand(numsources*numbins)*len(distribution))
         indices=indices.astype(int)
@@ -359,7 +411,7 @@ def generate_random_S(numsources,numbins,distribution=[]):
     else:
         S=abs(around(random.normal(2,1,size=[numsources,numbins])))
         return S
-    
+
 def sort_S(S):
     indices1=argsort(mean(S,axis=1))
     S1=S[indices1]
@@ -368,38 +420,38 @@ def sort_S(S):
 def sort_R(R):
     if R.shape[1]==1:
         return R
-    
+
     indices1=argsort(R[:,0])
     R1=R[indices1,:]
-    
+
     indices2=argsort(R1[0])
     R2=R1[:,indices2]
     return R2
-     
+
 def calc_R_similarity(R1,R2):
     r1=sort_R(R1)
     r2=sort_R(R2)
-    
+
     diff=sum((r1-r2)**2)
     return diff
 
 def calc_S_similarity(S1,S2):
     s1=sort_S(S1)
     s2=sort_S(S2)
-    
+
     diff=sum((s1-s2)**2)
     return diff
 
 def tab_to_csv(filename):
     f = open(filename,'r')
     f2=open("%s.csv" % filename,'w')
-    
+
     firstline=True
     for line in f:
         bare=line.strip()
         if (bare != ""):
             data=bare.split()
-            
+
         csv_line=""
         for item in data:
             csv_line+=item + ","
@@ -408,8 +460,6 @@ def tab_to_csv(filename):
             firstline=False
         else:
             f2.write("\n%s" % (csv_line))
-    
+
     f.close()
     f2.close()
-   
-    
--- test.py
+++ test.py
@@ -1,29 +1,84 @@
 #!/usr/bin/env python
-import deconvolve as dec
-reload(dec)
 import plotting
 reload(plotting)
-import pylab
+import deconvolve as dec
+reload(dec)
 
+import pylab
+import numpy
 
 
 import matplotlib.pyplot as plt
+import shutil
+import os
  
  
- 
-def test():
-    #test1:
-    filename = "test1_D.txt"
-    print filename
-    outdir = "user_data/yksbSYmai8lLaz8HnTHl/"
+def test_deconvolve_from_file():
+    
+    Dfilename = "test1_D.txt"
+    outdir = "user_data/testing/"
+    subdir= "3_clones/"
+    if not os.path.exists(outdir+Dfilename):
+        shutil.copyfile(Dfilename,outdir+Dfilename)
+    if not os.path.exists(outdir+subdir):
+        os.makedirs(outdir+subdir)
+    print Dfilename
+   
+    
+    
     print outdir
-    costs,allS = dec.run_deconvolve_from_file(filename,outdir,numclones=2,progress_file="%sprogress.txt" % outdir,testing=True)
+    progress_file=outdir+"progress.txt"
+    
+    dec.run_deconvolve_from_file(outdir+Dfilename,outdir+subdir,numclones=3,progress_file="%sprogress.txt" % outdir,testing=True)
     
     
     
-test ()
+test_deconvolve_from_file ()
 
 
+def test_deconvolve():
+    Dfilename = "test1_D.txt"
+    outdir = "user_data/testing/"
+    subdir= "3_clones/"
+    if not os.path.exists(outdir+Dfilename):
+        shutil.copyfile(Dfilename,outdir+Dfilename)
+    if not os.path.exists(outdir+subdir):
+        os.makedirs(outdir+subdir)
+    print Dfilename
+   
+    
+    
+    print outdir
+    progress_file=outdir+"progress.txt"
+    
+
+
+
+
+    costs,best_S,best_R,count_occurrence = dec.deconvolve(dec.loadmatrix(outdir+Dfilename), numclones=3, testing=True, max_falling_iterations=15,progress_file=progress_file,outdir=outdir)
+    print costs.shape
+    print best_S.shape
+    print best_R.shape
+    print min(costs)
+    print costs
+    print count_occurrence
+#test_deconvolve()
+    
+def test2():
+    A=[0.0]*10
+    print A
+    
+    for i in xrange(10):
+        A[i]=(10-i)*10.0
+    A=numpy.array(A)
+    print A
+    index=numpy.argsort(A)
+    print A[index]
+    
+    
+#test2()
+        
+        
 def plotting_test():
     #
     #D=plotting.loadmatrix("test1_D.txt")
